8d8491cdcbeb09fb6aeba04e0ddd7fa5519e84dc
Vitalii Sventyi <vsventy@gmail.com>
2021-08-28 18:14:24 +0300

    fix backend for python 3.8

--- celery/backends/redis.py
+++ celery/backends/redis.py
@@ -18,7 +18,7 @@ from celery.utils.functional import dictfilter
 from celery.utils.log import get_logger
 from celery.utils.time import humanize_seconds
 
-from . import async, base
+from . import asynchronous, base
 
 try:
     import redis
@@ -39,7 +39,7 @@ E_LOST = 'Connection to Redis lost: Retry (%s/%s) %s.'
 logger = get_logger(__name__)
 
 
-class ResultConsumer(async.BaseResultConsumer):
+class ResultConsumer(asynchronous.BaseResultConsumer):
 
     _pubsub = None
 
@@ -87,7 +87,7 @@ class ResultConsumer(async.BaseResultConsumer):
             self._pubsub.unsubscribe(key)
 
 
-class RedisBackend(base.BaseKeyValueStoreBackend, async.AsyncBackendMixin):
+class RedisBackend(base.BaseKeyValueStoreBackend, asynchronous.AsyncBackendMixin):
     """Redis task result store."""
 
     ResultConsumer = ResultConsumer
